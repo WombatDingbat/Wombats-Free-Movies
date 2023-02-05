f71a64dba40af23f03193e0dfa0328c4f80c5042
WombatDingbat <93640301+WombatDingbat@users.noreply.github.com>
2023-02-05 01:00:40 +0000

    Add files via upload

---
+++ season-selection.js
@@ -0,0 +1,217 @@
+function season1() {
+    document.getElementById("season1").style.height = "50%";
+    document.getElementById("season1").style.width = "60%";
+    document.getElementById("season1").style.visibility = "visible";
+
+    document.getElementById("season2").style.width = "0";
+    document.getElementById("season2").style.height = "0";
+    document.getElementById("season2").style.visibility = "hidden";
+
+    document.getElementById("season3").style.width = "0";
+    document.getElementById("season3").style.height = "0";
+    document.getElementById("season3").style.visibility = "hidden";
+
+    document.getElementById("season4").style.width = "0";
+    document.getElementById("season4").style.height = "0";
+    document.getElementById("season4").style.visibility = "hidden";
+
+    document.getElementById("season5").style.width = "0";
+    document.getElementById("season5").style.height = "0";
+    document.getElementById("season5").style.visibility = "hidden";
+
+    document.getElementById("season6").style.width = "0";
+    document.getElementById("season6").style.height = "0";
+    document.getElementById("season6").style.visibility = "hidden";
+
+    document.getElementById("season7").style.width = "0";
+    document.getElementById("season7").style.height = "0";
+    document.getElementById("season7").style.visibility = "hidden";
+
+}
+
+function season2() {
+    document.getElementById("season1").style.height = "0%";
+    document.getElementById("season1").style.width = "0%";
+    document.getElementById("season1").style.visibility = "hidden";
+
+    document.getElementById("season2").style.width = "60%";
+    document.getElementById("season2").style.height = "50%";
+    document.getElementById("season2").style.visibility = "visible";
+
+    document.getElementById("season3").style.width = "0";
+    document.getElementById("season3").style.height = "0";
+    document.getElementById("season3").style.visibility = "hidden";
+
+    document.getElementById("season4").style.width = "0";
+    document.getElementById("season4").style.height = "0";
+    document.getElementById("season4").style.visibility = "hidden";
+
+    document.getElementById("season5").style.width = "0";
+    document.getElementById("season5").style.height = "0";
+    document.getElementById("season5").style.visibility = "hidden";
+
+    document.getElementById("season6").style.width = "0";
+    document.getElementById("season6").style.height = "0";
+    document.getElementById("season6").style.visibility = "hidden";
+
+    document.getElementById("season7").style.width = "0";
+    document.getElementById("season7").style.height = "0";
+    document.getElementById("season7").style.visibility = "hidden";
+
+}
+
+function season3() {
+    document.getElementById("season1").style.height = "0%";
+    document.getElementById("season1").style.width = "0%";
+    document.getElementById("season1").style.visibility = "hidden";
+
+    document.getElementById("season2").style.width = "0";
+    document.getElementById("season2").style.height = "0";
+    document.getElementById("season2").style.visibility = "hidden";
+
+    document.getElementById("season3").style.width = "60%";
+    document.getElementById("season3").style.height = "50%";
+    document.getElementById("season3").style.visibility = "visible";
+
+    document.getElementById("season4").style.width = "0";
+    document.getElementById("season4").style.height = "0";
+    document.getElementById("season4").style.visibility = "hidden";
+
+    document.getElementById("season5").style.width = "0";
+    document.getElementById("season5").style.height = "0";
+    document.getElementById("season5").style.visibility = "hidden";
+
+    document.getElementById("season6").style.width = "0";
+    document.getElementById("season6").style.height = "0";
+    document.getElementById("season6").style.visibility = "hidden";
+
+    document.getElementById("season7").style.width = "0";
+    document.getElementById("season7").style.height = "0";
+    document.getElementById("season7").style.visibility = "hidden";
+
+}
+
+function season4() {
+    document.getElementById("season1").style.height = "0%";
+    document.getElementById("season1").style.width = "0%";
+    document.getElementById("season1").style.visibility = "hidden";
+
+    document.getElementById("season2").style.width = "0";
+    document.getElementById("season2").style.height = "0";
+    document.getElementById("season2").style.visibility = "hidden";
+
+    document.getElementById("season3").style.width = "0";
+    document.getElementById("season3").style.height = "0";
+    document.getElementById("season3").style.visibility = "hidden";
+
+    document.getElementById("season4").style.width = "60%";
+    document.getElementById("season4").style.height = "50%";
+    document.getElementById("season4").style.visibility = "visible";
+
+    document.getElementById("season5").style.width = "0";
+    document.getElementById("season5").style.height = "0";
+    document.getElementById("season5").style.visibility = "hidden";
+
+    document.getElementById("season6").style.width = "0";
+    document.getElementById("season6").style.height = "0";
+    document.getElementById("season6").style.visibility = "hidden";
+
+    document.getElementById("season7").style.width = "0";
+    document.getElementById("season7").style.height = "0";
+    document.getElementById("season7").style.visibility = "hidden";
+
+}
+
+function season5() {
+    document.getElementById("season1").style.height = "0%";
+    document.getElementById("season1").style.width = "0%";
+    document.getElementById("season1").style.visibility = "hidden";
+
+    document.getElementById("season2").style.width = "0";
+    document.getElementById("season2").style.height = "0";
+    document.getElementById("season2").style.visibility = "hidden";
+
+    document.getElementById("season3").style.width = "0";
+    document.getElementById("season3").style.height = "0";
+    document.getElementById("season3").style.visibility = "hidden";
+
+    document.getElementById("season4").style.width = "0";
+    document.getElementById("season4").style.height = "0";
+    document.getElementById("season4").style.visibility = "hidden";
+
+    document.getElementById("season5").style.width = "60%";
+    document.getElementById("season5").style.height = "50%";
+    document.getElementById("season5").style.visibility = "visible";
+
+    document.getElementById("season6").style.width = "0";
+    document.getElementById("season6").style.height = "0";
+    document.getElementById("season6").style.visibility = "hidden";
+
+    document.getElementById("season7").style.width = "0";
+    document.getElementById("season7").style.height = "0";
+    document.getElementById("season7").style.visibility = "hidden";
+
+}
+
+function season6() {
+    document.getElementById("season1").style.height = "0%";
+    document.getElementById("season1").style.width = "0%";
+    document.getElementById("season1").style.visibility = "hidden";
+
+    document.getElementById("season2").style.width = "0";
+    document.getElementById("season2").style.height = "0";
+    document.getElementById("season2").style.visibility = "hidden";
+
+    document.getElementById("season3").style.width = "0";
+    document.getElementById("season3").style.height = "0";
+    document.getElementById("season3").style.visibility = "hidden";
+
+    document.getElementById("season4").style.width = "0";
+    document.getElementById("season4").style.height = "0";
+    document.getElementById("season4").style.visibility = "hidden";
+
+    document.getElementById("season5").style.width = "0";
+    document.getElementById("season5").style.height = "0";
+    document.getElementById("season5").style.visibility = "hidden";
+
+    document.getElementById("season6").style.width = "60%";
+    document.getElementById("season6").style.height = "50%";
+    document.getElementById("season6").style.visibility = "visible";
+
+    document.getElementById("season7").style.width = "0";
+    document.getElementById("season7").style.height = "0";
+    document.getElementById("season7").style.visibility = "hidden";
+
+}
+
+function season7() {
+    document.getElementById("season1").style.height = "0%";
+    document.getElementById("season1").style.width = "0%";
+    document.getElementById("season1").style.visibility = "hidden";
+
+    document.getElementById("season2").style.width = "0";
+    document.getElementById("season2").style.height = "0";
+    document.getElementById("season2").style.visibility = "hidden";
+
+    document.getElementById("season3").style.width = "0";
+    document.getElementById("season3").style.height = "0";
+    document.getElementById("season3").style.visibility = "hidden";
+
+    document.getElementById("season4").style.width = "0";
+    document.getElementById("season4").style.height = "0";
+    document.getElementById("season4").style.visibility = "hidden";
+
+    document.getElementById("season5").style.width = "0";
+    document.getElementById("season5").style.height = "0";
+    document.getElementById("season5").style.visibility = "hidden";
+
+    document.getElementById("season6").style.width = "0";
+    document.getElementById("season6").style.height = "0";
+    document.getElementById("season6").style.visibility = "hidden";
+
+    document.getElementById("season7").style.width = "60%";
+    document.getElementById("season7").style.height = "50%";
+    document.getElementById("season7").style.visibility = "visible";
+
+}
+
